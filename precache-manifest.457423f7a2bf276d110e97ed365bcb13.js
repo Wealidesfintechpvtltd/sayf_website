78dfbdda285a27a09b40f6a17fc1b4bf689af099
Rachit Agarwal <rachitrajeshagarwal8484@gmail.com>
2022-01-02 14:28:47 +0530

    Updates

---
+++ precache-manifest.457423f7a2bf276d110e97ed365bcb13.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "214a212bce0dcd5f4f2f",
+    "url": "/sayf_website/static/css/main.504095a0.chunk.css"
+  },
+  {
+    "revision": "214a212bce0dcd5f4f2f",
+    "url": "/sayf_website/static/js/main.214a212b.chunk.js"
+  },
+  {
+    "revision": "d032fe871d4c63fbc256",
+    "url": "/sayf_website/static/js/1.d032fe87.chunk.js"
+  },
+  {
+    "revision": "214dcf51e410c5368580",
+    "url": "/sayf_website/static/js/2.214dcf51.chunk.js"
+  },
+  {
+    "revision": "95adb95f7da893e84ef0",
+    "url": "/sayf_website/static/js/runtime~main.95adb95f.js"
+  },
+  {
+    "revision": "d59c9acdd0800375396b2d87f186f7e1",
+    "url": "/sayf_website/static/media/7.d59c9acd.png"
+  },
+  {
+    "revision": "ef6bb486a3ffb7fbb7b433c7b37fc219",
+    "url": "/sayf_website/static/media/s_logo.ef6bb486.png"
+  },
+  {
+    "revision": "53ce77ad6121e144331233c012344da3",
+    "url": "/sayf_website/static/media/1.53ce77ad.png"
+  },
+  {
+    "revision": "bd773034b790a2031563f14955645789",
+    "url": "/sayf_website/static/media/2.bd773034.png"
+  },
+  {
+    "revision": "e77e5d858ba28a3d5b5dff6b8d09d98a",
+    "url": "/sayf_website/static/media/4.e77e5d85.png"
+  },
+  {
+    "revision": "c4010e4e7934a11349c62cf7b52448a0",
+    "url": "/sayf_website/static/media/5.c4010e4e.png"
+  },
+  {
+    "revision": "4dd5664f23c366710d9ea200069a517a",
+    "url": "/sayf_website/static/media/6.4dd5664f.png"
+  },
+  {
+    "revision": "0caa3ad7f5659da756457d0dc3745a3a",
+    "url": "/sayf_website/static/media/10.0caa3ad7.png"
+  },
+  {
+    "revision": "13c5c1b8251d9fe91a68d61344223f10",
+    "url": "/sayf_website/static/media/12.13c5c1b8.png"
+  },
+  {
+    "revision": "8948008fc8d1f9b776f45b9cd1551f8f",
+    "url": "/sayf_website/static/media/Svid.8948008f.gif"
+  },
+  {
+    "revision": "8422ae909f345727d17f7b8f32fc0f21",
+    "url": "/sayf_website/static/media/paytm.8422ae90.png"
+  },
+  {
+    "revision": "964224c0655ddaceb655d0a5bf4a3fe4",
+    "url": "/sayf_website/static/media/Augmont.964224c0.png"
+  },
+  {
+    "revision": "520d34bcf694ac66a14c7252af16c2dd",
+    "url": "/sayf_website/static/media/stores.520d34bc.png"
+  },
+  {
+    "revision": "8c044679d0db56a8688e560a020a4e3f",
+    "url": "/sayf_website/static/media/dailyinvestment.8c044679.png"
+  },
+  {
+    "revision": "6a038fc039d5fd0c61f5aa87ee731f0d",
+    "url": "/sayf_website/static/media/roundups.6a038fc0.png"
+  },
+  {
+    "revision": "a9a7c66d04848886ab0b7af2aa493b33",
+    "url": "/sayf_website/static/media/sayf_news.a9a7c66d.svg"
+  },
+  {
+    "revision": "10d55f0ba056776afe4e555cd36cb029",
+    "url": "/sayf_website/static/media/gplay.10d55f0b.png"
+  },
+  {
+    "revision": "615e4f5effe322a18d266d9fbd70a2cb",
+    "url": "/sayf_website/static/media/chrome.615e4f5e.png"
+  },
+  {
+    "revision": "4f343a2b5e4a623fd2885b0a711c3373",
+    "url": "/sayf_website/static/media/screen.4f343a2b.png"
+  },
+  {
+    "revision": "b42daa71b7014acea8f74fa46404a8ab",
+    "url": "/sayf_website/static/media/activate.b42daa71.png"
+  },
+  {
+    "revision": "a374fc18d4e44ef207f3313904744a1f",
+    "url": "/sayf_website/static/media/initiate.a374fc18.png"
+  },
+  {
+    "revision": "13c5c1b8251d9fe91a68d61344223f10",
+    "url": "/sayf_website/static/media/11.13c5c1b8.png"
+  },
+  {
+    "revision": "4a86591b61fb908b1e2aa8c72b2c29a8",
+    "url": "/sayf_website/static/media/forget.4a86591b.gif"
+  },
+  {
+    "revision": "d38d9c3f414896fbc496d743b9b534f5",
+    "url": "/sayf_website/static/media/landing.d38d9c3f.mp4"
+  },
+  {
+    "revision": "91b39cd6b8490b3410b7b96b9fcbf700",
+    "url": "/sayf_website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d55bb21404d3f9bf09c1e10eb87055c8.js"
+  "/sayf_website/precache-manifest.457423f7a2bf276d110e97ed365bcb13.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/sayf_website/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
