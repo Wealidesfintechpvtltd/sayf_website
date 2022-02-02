cf34d6b41aaae1f4e951ecf020c951a09b352852
Rachit Agarwal <rachitrajeshagarwal8484@gmail.com>
2022-02-02 13:21:09 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5dc08d2c833cec8313e8875840e5d873.js"
+  "/precache-manifest.77c54be7e50bf4f4c9c66fef563047a7.js"
 );
 
 workbox.clientsClaim();
